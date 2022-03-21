6165e5432e05a9d5622cca1992fc2868dc4e938a
Alexis Caillé <alexiscaille97@gmail.com>
2022-03-21 18:35:06 +0100

    21/03

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,49 +6,40 @@ use App\Entity\Recettes;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController
 {
     /**
      * @var Environment
+     * @var RecettesRepository
      */
     private $twig;
+    private $repository;
 
     public function __construct(Environment $twig)
     {
         $this->twig = $twig;
     }
+    public function __construct(RecettesRepository $repository){
+
+    }
+    
     public function index(): Response
     {
         return new Response($this->twig->render('pages/home.html.twig'));
     }
-    public function recettes(): Response
+
+    public function recettes(ManagerRegistry $doctrine): Response
     {
-
-
-        $recette = new Recettes();
-        $recette->setTitreRecette("Chili con Carne")
-        ->setDescriptionRecette("Ingrédients :
-        -300 g de viande hachée
-        -230g de chaire de tomate
-        -250 g d’haricots rouges
-        -2 sachets 1 personne de riz
-        -140 g de maïs
-        -épices chili
-        Conseils de préparation :
-        -égouttez les haricots rouges avant de les mélanger avec la viande
-        ")
-        ->setPrixRecette(4.20);
-        $em = $this->getDoctrine()->getManager();
-        $em-> persist($recette);
-        $em->flush();
-
-
+        $recettes = $this->repository->find(1);
+        dump($recettes);
 
         return new Response($this->twig->render('pages/recettes.html.twig'));
-        
     }
+
+
+
     public function bonsplans(): Response
     {
         return new Response($this->twig->render('pages/bonsplans.html.twig'));
