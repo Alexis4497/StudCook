95765e9cd636695dc508a1aeb301a442cca99f32
Alexis Caillé <alexiscaille97@gmail.com>
2022-04-12 12:29:12 +0200

    12/04/2022

--- src/Controller/RecettesController.php
+++ src/Controller/RecettesController.php
@@ -6,25 +6,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\RecettesRepository;
+use App\Entity\Recettes;
+use Doctrine\Persistence\ManagerRegistry;
 
 class RecettesController extends AbstractController
 {
       
     /**
+     * Class RecettesController
+     * @package App\Controller
      * @param RecettesRepository $repository
-     * @Route("/recettes", name="recettes.index")
+     * @Route("/recettes", name="recettes")
      * @return Response
      */
    
 
-    public function index(RecettesRepository $repository): Response
+    public function recettes(RecettesRepository $repository)
     {
       
-        $recette = $repository->findAll();
-        
-        
         return $this->render('pages/recettes.html.twig', [
-            'recette' => '$recette'
+            'recettes' => $repository -> findAll([]) 
         ]);
     }
 }
